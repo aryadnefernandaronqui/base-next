e60a45434328f33de097c18da9f988d7fe4e851c
Aryadne Fernanda Ronqui <100657052+aryadnefernandaronqui@users.noreply.github.com>
2025-02-04 08:22:39 -0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HomePage from "./home/page";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col min-h-screen w-full">
+      <HomePage/>
+    </main>
+  );
+}
